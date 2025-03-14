74569295e7b1c70d7aba063d82589fa40fc42d46
Jakub <lowisjakub@gmail.com>
2025-03-14 11:53:00 +0000

    Initial Commit

---
+++ JL_RR_MD_CA/JL_RR_MD_CA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JL_RR_MD_CA
+//
+//  Created by Student on 14/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JL_RR_MD_CA/JL_RR_MD_CA/JL_RR_MD_CAApp.swift
@@ -0,0 +1,17 @@
+//
+//  JL_RR_MD_CAApp.swift
+//  JL_RR_MD_CA
+//
+//  Created by Student on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct JL_RR_MD_CAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
