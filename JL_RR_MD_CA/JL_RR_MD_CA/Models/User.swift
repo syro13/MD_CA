25e5ffb92476ec18d843160584f4ff6a3ca2d9fc
Jakub <raveenarathnam02@gmail.com>
2025-04-17 17:06:58 +0100

    Implement User.swift

---
+++ JL_RR_MD_CA/JL_RR_MD_CA/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  JL_RR_MD_CA
+//
+//  Created by Tanmay Bhande on 16/04/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let email: String
+    let phone: String
+    let password: String
+}
