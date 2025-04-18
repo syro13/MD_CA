c90beb5e5993354ee3121c3ace234938f54af447
Jakub <raveenarathnam02@gmail.com>
2025-04-18 01:48:59 +0100

    Create RecipeSggestionsView.swift

---
+++ JL_RR_MD_CA/JL_RR_MD_CA/Views/Recipes/RecipeSuggestionsView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeSuggestionsView.swift
+//  JL_RR_MD_CA
+//
+//  Created by Tanmay Bhande on 18/04/25.
+//
+
+import SwiftUI
+
+struct RecipeSuggestionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeSuggestionsView()
+}
