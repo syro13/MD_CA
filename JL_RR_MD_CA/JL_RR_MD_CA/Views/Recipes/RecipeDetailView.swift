3dbf480ac925ebc16b79407e97648f9311ba74c3
Jakub <raveenarathnam02@gmail.com>
2025-04-18 01:48:39 +0100

    Create RecipeDetailView.swift

---
+++ JL_RR_MD_CA/JL_RR_MD_CA/Views/Recipes/RecipeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailView.swift
+//  JL_RR_MD_CA
+//
+//  Created by Tanmay Bhande on 18/04/25.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeDetailView()
+}
