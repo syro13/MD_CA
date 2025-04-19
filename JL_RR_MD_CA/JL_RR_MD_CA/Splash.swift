0b1928923212f43155746b2e7951b7552d3a6c1b
Jakub <95759959+syro13@users.noreply.github.com>
2025-04-19 19:12:30 +0100

    Update Splash screen to match design

--- JL_RR_MD_CA/JL_RR_MD_CA/Splash.swift
+++ JL_RR_MD_CA/JL_RR_MD_CA/Splash.swift
@@ -12,25 +12,20 @@ struct Splash: View {
 
     var body: some View {
         if isActive {
-            Dashboard() // Main content after splash
+            OnBoarding()
         } else {
-            VStack {
-                Text("🍔") // Splash emoji or logo
-                    .font(.system(size: 100))
-                Text("Welcome to FoodieApp")
-                    .font(.title)
-                    .fontWeight(.bold)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.white)
-            .onAppear {
-                // Wait for 2 seconds before switching view
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    withAnimation {
-                        isActive = true
+                Image("splash_image")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color(red: 40/255, green: 39/255, blue: 39/255))
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation {
+                                isActive = true
+                            }
+                        }
                     }
-                }
-            }
         }
     }
 }
