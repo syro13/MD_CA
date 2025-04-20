1f241bed0c453331b3c3d7cbc1ed4f033f921f05
Jakub <raveenarathnam02@gmail.com>
2025-04-21 00:37:45 +0100

    Update Recipe.swift

--- JL_RR_MD_CA/JL_RR_MD_CA/Models/Recipe.swift
+++ JL_RR_MD_CA/JL_RR_MD_CA/Models/Recipe.swift
@@ -15,10 +15,6 @@ struct Recipe: Identifiable, Decodable {
     let missedIngredients: [Ingredient]
 }
 
-struct Ingredient: Identifiable, Decodable {
-    let id: Int
-    let name: String
-    let original: String
-    let image: String
-}
+
+
 
