57d1e0b8547349e1704c9328b2f77cf9c6137b24
Jakub <raveenarathnam02@gmail.com>
2025-04-21 00:37:34 +0100

    Implement RecipeSummary.swift

---
+++ JL_RR_MD_CA/JL_RR_MD_CA/Models/RecipeSummary.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeSummary.swift
+//  JL_RR_MD_CA
+//
+//  Created by Tanmay Bhande on 19/04/25.
+//
+
+import Foundation
+
+struct RecipeSummary: Decodable {
+    let summary: String
+}
+
